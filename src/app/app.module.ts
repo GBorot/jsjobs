5e9801e279bee9540e38764c072c26ba8e45e460
Gautier Borot <g.borot@gmail.com>
2020-04-01 09:48:39 +0200

    imported HttpClientModule because of Angular 5 or higher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { JobListComponent } from './job-list/job-list.component';
 
 @NgModule({
   declarations: [AppComponent, SearchComponent, JobListComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
